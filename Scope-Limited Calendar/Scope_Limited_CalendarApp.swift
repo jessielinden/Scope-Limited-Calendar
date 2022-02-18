fc264a93089ddf18c994e88461fa0db5c4117bcc
Jessica Linden <jessielinden1@gmail.com>
2022-02-18 12:11:56 -0500

    Initial Commit

---
+++ Scope-Limited Calendar/Scope_Limited_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scope_Limited_CalendarApp.swift
+//  Scope-Limited Calendar
+//
+//  Created by Jessica Linden on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Scope_Limited_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
